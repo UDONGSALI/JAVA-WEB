a08fbdeffd67be5c92acc17c86d9ca99ce264c80
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-06-13 08:53:18 +0900

    pro05

---
+++ pro05/src/main/java/sec01/ex01/SecondServlet.java
@@ -0,0 +1,24 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.rmi.ServerException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet {
+	
+	public void init() throws ServletException{
+		System.out.println("init 메서드 호출2");
+	}
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException{
+		System.out.println("doGet 메서드 호출2");
+	}
+	
+	public void destroy() {
+		System.out.println("destroy 메서드 호출2");
+	}
+
+}
---
+++ pro05/src/main/java/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,58 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * Default constructor. 
+     */
+    public ThirdServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("ThirdServlet init메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println("ThirdServlet doPost 메서드 호출");
+	}
+
+}
